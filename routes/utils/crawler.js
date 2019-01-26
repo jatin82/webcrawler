881be131b5533c24ab1d0c767132cfa5227a219c
Jatin Sokhal <jatin.sokhal3@gmail.com>
2019-01-26 17:03:45 +0530

    Added corrections

--- routes/utils/crawler.js
+++ routes/utils/crawler.js
@@ -1,7 +1,6 @@
 
 const rp = require('request-promise');
 
-new Crawler()
 var crawler = {
       fetchItem : function(uri){
         return rp({url:uri,timeout:420 * 1000,maxRedirects:10,encoding:'utf8',gzip:true,resolveWithFullResponse: true})
